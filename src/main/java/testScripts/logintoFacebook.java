fe4e9a7708983083d3f2c112cfded13170e7838d
Madhusudhankn24 <madhusudhankn24@gmail.com>
2023-12-14 12:39:30 +0530

    jenkins commit

---
+++ src/main/java/testScripts/logintoFacebook.java
@@ -0,0 +1,17 @@
+package testScripts;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class logintoFacebook {
+    private static WebDriver driver;
+
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().deleteAllCookies();
+        driver.get("https://www.facebook.com");
+    }
+}
