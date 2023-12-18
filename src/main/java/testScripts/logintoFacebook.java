0bf5d91b602e5222e92251fbc1edad02472d433d
Madhusudhankn24 <madhusudhankn24@gmail.com>
2023-12-18 14:37:10 +0530

    Selenium with Jenkins

--- src/main/java/testScripts/logintoFacebook.java
+++ src/main/java/testScripts/logintoFacebook.java
@@ -3,15 +3,21 @@ package testScripts;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 public class logintoFacebook {
     private static WebDriver driver;
 
-    public static void main(String[] args) {
+    @Test(priority = 0)
+    public void test1(){
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().deleteAllCookies();
         driver.get("https://www.facebook.com");
+    }
+    @Test(priority = 1)
+    public void test2(){
+        System.out.println("This test executing through jenkins");
     }
 }
