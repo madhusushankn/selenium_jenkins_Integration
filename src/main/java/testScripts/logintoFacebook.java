a36f267dfce8270b36921ff737d6ae0835eace56
madhusudhankn <153803765+madhusushankn@users.noreply.github.com>
2023-12-18 15:23:19 +0530

    Update logintoFacebook.java

--- src/main/java/testScripts/logintoFacebook.java
+++ src/main/java/testScripts/logintoFacebook.java
@@ -16,8 +16,6 @@ public class logintoFacebook {
         driver.manage().deleteAllCookies();
         driver.get("https://www.facebook.com");
     }
-    /*
-    */
     @Test(priority = 1)
     public void test2(){
         System.out.println("This test executing through jenkins");
